80b29d9a760a75ff7477421071c4dff40a16c53b
devbug <hbran6@gmail.com>
2021-03-02 20:44:19 -0600

    Se corrige error en el valor absoluto del metodo de biseccion, se modifica la nueva funcion a evaluar

--- biseccion.cpp
+++ biseccion.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-float a, b, p, tolerancia, FA, FP;
+double a, b, p, tolerancia, FA, FP;
 int i, iteraciones, num_funcion, inicio_y = 4, inicio_y_encabezado = 1;
 bool exito;
 
@@ -26,51 +26,11 @@ main(){
 	ingresar_datos();
 	system("cls");
 	
-//	set_variables(1);
-iteraciones = 100;
-tolerancia = pow(10, -4);
-if(num_funcion == 1){
-		a = 0;
-		b = 1;
-	}
-	if(num_funcion == 2){
-		a = 0;
-		b = 1;
-	}
-	if(num_funcion == 3){
-		a = -1;
-		b = 0;
-	}
-	if(num_funcion == 4){
-		a = 0.2;
-		b = 0.3;
-	}
-	if(num_funcion == 5){
-		a = 1.2;
-		b = 1.3;
-	}
-	ejecutar_biseccion();
-//	set_variables(0);
-tolerancia = pow(10, -8);
-if(num_funcion == 1){
-		a = 0;
-		b = 1;
-	}
-	if(num_funcion == 2){
-		a = 0;
-		b = 1;
-	}
-	if(num_funcion == 3){
-		a = -1;
-		b = 0;
-	}
-	if(num_funcion == 4){
-		a = 0.2;
-		b = 0.3;
-	}
-	if(num_funcion == 5){
-		a = 1.2;
-		b = 1.3;
+	iteraciones = 100;
+	tolerancia = 0.000001;
+	if(num_funcion == 1){
+		a = -12;
+		b = -10;
 	}
 	ejecutar_biseccion();
 	getch();
@@ -85,13 +45,12 @@ void ejecutar_biseccion(){
 		p = (a+b)/2;
 		FP = ejecutar_funcion(p);
 		mostrar_iteracion();
-		if(FP == 0 || (abs(b-a)/2)<tolerancia){
+		if(FP == 0 || abs((b-a)/2)<tolerancia){
 			exito = 1;
 			msj_salida(1);
 		}
 		if(exito == 0){
 			i++;
-//			printf("\nFA:%4.15f", FA);
 			if(FA * FP > 0){
 				a = p;
 				FA = FP;
@@ -102,77 +61,27 @@ void ejecutar_biseccion(){
 	}
 	if(exito == false)
 		msj_salida(0);
-//	do{
-//		p = (a+b)/2;
-//		FP = ejecutar_funcion(p);
-//		if(FP == 0 || (abs(b-a)/2)<tolerancia){
-//			exito = true;
-//			msj_salida(1);
-//		}else{
-//			mostrar_iteracion();
-//			i++;
-//			if(FA * FP > 0){
-//				a = p;
-//				FA = FP;
-//			}else{
-//				b = p;
-//			}
-//		}		
-//	}while((i <= iteraciones) && exito == false);
-//	if(exito == false)
-//		msj_salida(0);
 }
 
 float ejecutar_funcion(float x){
 	if(num_funcion == 1)
-		return x - pow(2, x*-1);
-		
-	if(num_funcion == 2)
-		return pow(2.71828, x) - pow(x, 2) + 3*x - 2;
-		
-	if(num_funcion == 3)
-		return 2 * x * cos(x) - pow((x+1), 2);
-		
-	if(num_funcion == 4 || num_funcion == 5)
-		return x * cos(x) - pow(2 * x, 2) + 3 * x - 1;
+		return (pow(2, x)) - (6 * cos(x));
 }
 
 void ingresar_datos(){
 	cout<<"Seleccione la funcion que desea ejecutar: "<<endl;
-	cout<<"1. f(x) = x - 2^-x = 0 Intervalos [0, 1]"<<endl;
-	cout<<"2. f(x) = e^x - x^2 + 3x - 2 = 0 Intervalos [0, 1]"<<endl;
-	cout<<"3. f(x) = 2x * cos(x) - (x+1)^2 = 0 Intervalos [-1, 0]"<<endl;
-	cout<<"4. f(x) = x * cos(x) - 2x^2 + 3x - 1 = 0 Intervalos [0.2, 0.3]"<<endl;
-	cout<<"5. f(x) = x * cos(x) - 2x^2 + 3x - 1 = 0 Intervalos [1.2, 1.3]"<<endl;
+	cout<<"1. f(x) = 2^x - 6cos(x) Intervalos [-12, -10]"<<endl;
 	cin>>num_funcion;
 }
 
 void set_variables(bool primera_ejecucion){
-	tolerancia = pow(10, -8);
-	if(!primera_ejecucion)
-		tolerancia = pow(10, -4);
+	tolerancia = pow(10, -15);
 		
 	iteraciones = 100;
 	
 	if(num_funcion == 1){
-		a = 0;
-		b = 1;
-	}
-	if(num_funcion == 2){
-		a = 0;
-		b = 1;
-	}
-	if(num_funcion == 3){
-		a = -1;
-		b = 0;
-	}
-	if(num_funcion == 4){
-		a = 0.2;
-		b = 0.3;
-	}
-	if(num_funcion == 5){
-		a = 1.2;
-		b = 1.3;
+		a = -12;
+		b = -10;
 	}
 }
 
@@ -186,7 +95,7 @@ void msj_salida(bool exito){
 }
 void mostrar_iteracion(){
 	gotoxy(1, inicio_y);printf("%i", i);
-	gotoxy(15, inicio_y);printf("%4.15f", FA*FP);
+	gotoxy(15, inicio_y);printf("%4.15f", a);
 	gotoxy(35, inicio_y);printf("%4.15f", b);
 	gotoxy(55, inicio_y);printf("%4.15f", p);
 	gotoxy(75, inicio_y);printf("%4.15f", FP);
