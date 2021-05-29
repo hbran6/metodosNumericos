e15e22de95a48e789adc8250e77ed2cb917f5e1d
devbug <hbran6@gmail.com>
2021-05-28 21:53:20 -0600

    Se agrega el metodo de Gauss-Seidel

---
+++ gaussSeidel.cpp
@@ -0,0 +1,98 @@
+/*
+Herber Bran
+Universidad Mariano Galvez de Guatemala
+Metodos Numericos
+2021-05-28
+
+Metodo de Gauss-Seidel
+*/
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+int n;
+float seidel(float m[], float aux[], int pos);
+float obtenerError(float xAnt[], float x[], int pos);
+
+int main(){
+	cout<<"Ingrese la cantidad de variables ";
+	cin>>n;
+	
+	float a[n][n], b[n], x[n], xAnt[n], aux[n], error[n], tolerancia;
+	int iteraciones = 0, maxIteraciones;
+	bool exito;
+	
+//	pedimos la matriz aumentada
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < n; j++){
+			cout<<"Valor en ["<<i<<", "<<j<<"] = ";
+			cin>>a[i][j];
+		}
+	}
+//	pedimos el vector del resultado
+	for(int i =0; i < n; i++){
+		cout<<"b["<<i<<"] = ";
+		cin>>b[i];
+	}
+	
+	cout<<"Ingrese la tolerancia: ";
+	cin>>tolerancia;
+	
+	cout<<"Cantidad maxima de iteraciones: ";
+	cin>>maxIteraciones;
+//	pedimos los valores iniciales
+	for(int i = 0; i < n; i++){
+		cout<<"variable "<<i<<" = ";
+		cin>>xAnt[i];
+	}
+	
+	do{
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				//se agrega el valor de la posicion de la matriz aumentada a un vector para hacer el calculo en la funcion seidel
+				aux[j] = a[i][j];
+			}
+			x[i] = (1 / a[i][i]) * (b[i] - seidel(aux, xAnt, i));
+			error[i] = obtenerError(x, xAnt, i);
+			//se cambia el valor anterior por el nuevo para el siguiente calculo
+			xAnt[i] = x[i];
+		}
+		
+		for(int i = 0; i < n; i++){
+			if(error[i] <= tolerancia){
+				exito = true;
+				break;
+			}else{
+				exito = false;
+				break;
+			}
+		}
+		cout<<endl<<"Iteracion "<<iteraciones<<endl;
+		for(int i = 0; i < n; i++){
+			cout<<"x"<<i + 1<<" = "<<x[i]<<endl;
+		}
+		iteraciones++;
+	}while(exito == false && iteraciones < maxIteraciones);
+	
+	cout<<endl<<"Terminando "<<endl<< "Iteraciones: "<<iteraciones - 1<<endl;
+	for(int i = 0; i < n; i++){
+		cout<<"X"<<i+1<<" valor aproximado = "<<x[i]<<endl;
+	}
+	return 1;
+}
+
+float seidel(float m[], float aux[], int pos){
+	float temp = 0;
+	for(int i = 0; i < n; i++){
+		if(pos != i){
+			temp = temp + m[i] * aux[i];
+		}
+	}
+	return temp;
+}
+float obtenerError(float v1[], float v2[], int pos){
+	float error;
+	error = fabs((v1[pos] - v2[pos]) / v1[pos]);
+	return error;
+}
